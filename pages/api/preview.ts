c83f33c89540fc00aba61e26ee36f17e3fcd29b0
Markus Haack <markus.haack@gmail.com>
2022-04-21 07:42:52 +0200

    Fix preview

--- pages/api/preview.ts
+++ pages/api/preview.ts
@@ -5,8 +5,6 @@ export default async function preview(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  // Check the secret and next parameters
-  // This secret should only be known to this API route and the CMS
   if (
     req.query.secret !== process.env.STORYBLOK_PREVIEW_SECRET ||
     !req.query.slug
@@ -14,19 +12,14 @@ export default async function preview(
     return res.status(401).json({ message: 'Invalid token' })
   }
 
-  // Fetch the headless CMS to check if the provided `slug` exists
   const macroImage = await getPreviewMacroImageBySlug(req.query.slug)
-
-  // If the slug doesn't exist prevent preview mode from being enabled
   if (!macroImage) {
     return res.status(401).json({ message: 'Invalid slug' })
   }
 
-  // Enable Preview Mode by setting the cookies
-  res.setPreviewData({})
 
-  // Redirect to the path from the fetched post
-  // We don't redirect to req.query.slug as that might lead to open redirect vulnerabilities
-  res.writeHead(307, { Location: `/images/${macroImage?.MacroimageItem?.slug}` })
+  res.setPreviewData({})
+  const slug = macroImage?.MacroimageItem['full_slug']
+  res.writeHead(307, { Location: `/images/${slug}` })
   res.end()
 }
