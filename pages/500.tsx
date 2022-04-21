b436398072e8ae5f8e0e5020146dcbde71f7d74f
Markus Haack <markus.haack@gmail.com>
2022-04-21 14:31:54 +0200

    Add back to home linke

--- pages/500.tsx
+++ pages/500.tsx
@@ -1,5 +1,5 @@
 import Head from 'next/head'
-import Link from 'next/link'
+import BackLink from '../components/back-link'
 import Layout from '../components/layout'
 
 export default function Custom404() {
@@ -10,7 +10,7 @@ export default function Custom404() {
       </Head>
       <div className="py-28 text-center">
         <h1 className="mb-10 text-4xl">500 - here are no photos ...</h1>
-        <Link href="/">Back to Gallery</Link>
+        <BackLink />
       </div>
     </Layout>
   )
