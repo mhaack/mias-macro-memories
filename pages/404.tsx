36a345f45074cdbe7224dab36557c893fb23f308
Markus Haack <markus.haack@gmail.com>
2022-04-21 14:05:55 +0200

    Add custom 404 page

---
+++ pages/404.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+import Link from 'next/link'
+import Layout from '../components/layout'
+
+export default function Custom404() {
+  return (
+    <Layout>
+      <Head>
+        <title>Mia's Macro Memories</title>
+      </Head>
+      <div className="py-28 text-center">
+        <h1 className="mb-10 text-4xl">404 - here are no photos ...</h1>
+        <Link href="/">Back to Gallery</Link>
+      </div>
+    </Layout>
+  )
+}
