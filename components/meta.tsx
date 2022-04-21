19c28da01549e58278c8ece9cc44185fa5025fcb
Markus Haack <markus.haack@gmail.com>
2022-04-21 14:45:20 +0200

    Add plausible script

--- components/meta.tsx
+++ components/meta.tsx
@@ -8,8 +8,9 @@ const Meta = () => {
       <link
         href="https://fonts.googleapis.com/css2?family=Teko&display=swap"
         rel="stylesheet"
-      />
+      />    
       <link rel="icon" href="/favicon.ico" />
+      <script defer data-domain="mmm.pictures" src="/js/script.js"></script>
     </Head>
   )
 }
